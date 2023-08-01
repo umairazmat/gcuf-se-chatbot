95dc8abe815db9ee5ae4aa7a3abaf5c3e0666226
Umair Azmat <umairazmat8260.com>
2023-08-01 16:06:27 +0500

    GUI Updated 🔥

--- app.py
+++ app.py
@@ -1,84 +1,111 @@
-from tkinter import *
-from chat import get_response, bot_name
+# Pytorch Desktop Application
 
-BG_GRAY = "#FFFFFF"
-BG_COLOR = "#C90000"
-TEXT_COLOR = "#FFFB00"
+# from tkinter import *
+# from chat import get_response, bot_name
 
-FONT = "Helvetica 14"
-FONT_BOLD = "Helvetica 13 bold"
+# BG_GRAY = "#FFFFFF"
+# BG_COLOR = "#C90000"
+# TEXT_COLOR = "#FFFB00"
 
-class ChatApplication:
+# FONT = "Helvetica 14"
+# FONT_BOLD = "Helvetica 13 bold"
+
+# class ChatApplication:
     
-    def __init__(self):
-        self.window = Tk()
-        self._setup_main_window()
+#     def __init__(self):
+#         self.window = Tk()
+#         self._setup_main_window()
         
-    def run(self):
-        self.window.mainloop()
+#     def run(self):
+#         self.window.mainloop()
         
-    def _setup_main_window(self):
-        self.window.title("GCUF SE Chatbot")
-        self.window.resizable(width=False, height=False)
-        self.window.configure(width=570, height=550, bg=BG_COLOR)
+#     def _setup_main_window(self):
+#         self.window.title("GCUF SE Chatbot")
+#         self.window.resizable(width=False, height=False)
+#         self.window.configure(width=570, height=550, bg=BG_COLOR)
         
-        # head label
-        head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,
-                           text="Welcome  To GCUF SE  Automate ChatBot ", font=FONT_BOLD, pady=10)
-        head_label.place(relwidth=1)
+#         # head label
+#         head_label = Label(self.window, bg=BG_COLOR, fg=TEXT_COLOR,
+#                            text="Welcome  To GCUF SE  Automate ChatBot ", font=FONT_BOLD, pady=10)
+#         head_label.place(relwidth=1)
         
-        # tiny divider
-        line = Label(self.window, width=450, bg=BG_GRAY)
-        line.place(relwidth=1, rely=0.07, relheight=0.012)
+#         # tiny divider
+#         line = Label(self.window, width=450, bg=BG_GRAY)
+#         line.place(relwidth=1, rely=0.07, relheight=0.012)
         
-        # text widget
-        self.text_widget = Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR,
-                                font=FONT, padx=5, pady=5)
-        self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
-        self.text_widget.configure(cursor="arrow", state=DISABLED)
+#         # text widget
+#         self.text_widget = Text(self.window, width=20, height=2, bg=BG_COLOR, fg=TEXT_COLOR,
+#                                 font=FONT, padx=5, pady=5)
+#         self.text_widget.place(relheight=0.745, relwidth=1, rely=0.08)
+#         self.text_widget.configure(cursor="arrow", state=DISABLED)
         
-        # scroll bar
-        scrollbar = Scrollbar(self.text_widget)
-        scrollbar.place(relheight=1, relx=0.974)
-        scrollbar.configure(command=self.text_widget.yview)
+#         # scroll bar
+#         scrollbar = Scrollbar(self.text_widget)
+#         scrollbar.place(relheight=1, relx=0.974)
+#         scrollbar.configure(command=self.text_widget.yview)
         
-        # bottom label
-        bottom_label = Label(self.window, bg=BG_GRAY, height=80)
-        bottom_label.place(relwidth=1, rely=0.825)
+#         # bottom label
+#         bottom_label = Label(self.window, bg=BG_GRAY, height=80)
+#         bottom_label.place(relwidth=1, rely=0.825)
         
-        # message entry box
-        self.msg_entry = Entry(bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
-        self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
-        self.msg_entry.focus()
-        self.msg_entry.bind("<Return>", self._on_enter_pressed)
+#         # message entry box
+#         self.msg_entry = Entry(bottom_label, bg="#2C3E50", fg=TEXT_COLOR, font=FONT)
+#         self.msg_entry.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
+#         self.msg_entry.focus()
+#         self.msg_entry.bind("<Return>", self._on_enter_pressed)
         
-        # send button
-        send_button = Button(bottom_label, text="Send / Ask", font=FONT_BOLD, width=20, bg=BG_GRAY,
-                             command=lambda: self._on_enter_pressed(None))
-        send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
+#         # send button
+#         send_button = Button(bottom_label, text="Send / Ask", font=FONT_BOLD, width=20, bg=BG_GRAY,
+#                              command=lambda: self._on_enter_pressed(None))
+#         send_button.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
      
-    def _on_enter_pressed(self, event):
-        msg = self.msg_entry.get()
-        self._insert_message(msg, "You")
+#     def _on_enter_pressed(self, event):
+#         msg = self.msg_entry.get()
+#         self._insert_message(msg, "You")
         
-    def _insert_message(self, msg, sender):
-        if not msg:
-            return
+#     def _insert_message(self, msg, sender):
+#         if not msg:
+#             return
         
-        self.msg_entry.delete(0, END)
-        msg1 = f"{sender}: {msg}\n\n"
-        self.text_widget.configure(state=NORMAL)
-        self.text_widget.insert(END, msg1)
-        self.text_widget.configure(state=DISABLED)
+#         self.msg_entry.delete(0, END)
+#         msg1 = f"{sender}: {msg}\n\n"
+#         self.text_widget.configure(state=NORMAL)
+#         self.text_widget.insert(END, msg1)
+#         self.text_widget.configure(state=DISABLED)
         
-        msg2 = f"{bot_name}: {get_response(msg)}\n\n"
-        self.text_widget.configure(state=NORMAL)
-        self.text_widget.insert(END, msg2)
-        self.text_widget.configure(state=DISABLED)
+#         msg2 = f"{bot_name}: {get_response(msg)}\n\n"
+#         self.text_widget.configure(state=NORMAL)
+#         self.text_widget.insert(END, msg2)
+#         self.text_widget.configure(state=DISABLED)
         
-        self.text_widget.see(END)
+#         self.text_widget.see(END)
              
         
+# if __name__ == "__main__":
+#     app = ChatApplication()
+#     app.run()
+
+# Flask Web App
+
+from flask import Flask, render_template, request, jsonify
+from chat import get_response
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"]) 
+def index_get():
+    return render_template('base.html')
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    data = request.get_json()
+    if data is not None and "message" in data:
+        text = data["message"]
+        response = get_response(text)
+        message = {"answer": response}
+        return jsonify(message)
+    else:
+        return jsonify({"error": "Invalid request"}), 400
+
 if __name__ == "__main__":
-    app = ChatApplication()
-    app.run()
+    app.run(debug=True)
