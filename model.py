d5b60960f3dbdde25f2c98df566417e1a9b3143c
Umair Azmat <umairazmat8260.com>
2023-09-15 09:17:47 +0500

    changes

--- model.py
+++ model.py
@@ -1,20 +1,22 @@
-import torch
-import torch.nn as nn
+import torch #imports the PyTorch library,
+import torch.nn as nn # imports the nn module from PyTorch, which contains all 
+#the tools needed to define and construct neural networks.
 
 
-class NeuralNet(nn.Module):
-    def __init__(self, input_size, hidden_size, num_classes):
-        super(NeuralNet, self).__init__()
-        self.l1 = nn.Linear(input_size, hidden_size) 
+class NeuralNet(nn.Module): #This defines a new class called NeuralNet, which is a subclass of nn.Module.
+    def __init__(self, input_size, hidden_size, num_classes): #constructor (initializer) of the NeuralNet class
+        super(NeuralNet, self).__init__() #calls the constructor of the parent class nn.Module to properly initialize the NeuralNet class.
+        self.l1 = nn.Linear(input_size, hidden_size) #line creates a linear layer 
         self.l2 = nn.Linear(hidden_size, hidden_size) 
         self.l3 = nn.Linear(hidden_size, num_classes)
-        self.relu = nn.ReLU()
+        self.relu = nn.ReLU() #creates an activation function called ReLU (Rectified Linear Unit)
+        #most commonly used activation functions in neural networks.
     
-    def forward(self, x):
-        out = self.l1(x)
-        out = self.relu(out)
+    def forward(self, x): #the forward pass is the computation that takes the input data x and produces the output.
+        out = self.l1(x) #This line applies the first linear layer to the input x.
+        out = self.relu(out) #This line applies the ReLU activation function to the output of the first layer.
         out = self.l2(out)
         out = self.relu(out)
         out = self.l3(out)
         # no activation and no softmax at the end
-        return out
+        return out #This line returns the output of the neural network.
--- nltk_utils.py
+++ nltk_utils.py
@@ -1,11 +1,11 @@
-import numpy as np
-import nltk
+import numpy as np #NumPy is a powerful library for numerical computing in Python, and it provides support for large, multi-dimensional arrays and matrices, along with a collection of high-level mathematical functions.
+import nltk #Python library for NLP tasks, such as tokenization, stemming, lemmatization, part-of-speech tagging, and more.
 # nltk.download('punkt')
-from nltk.stem.porter import PorterStemmer
+from nltk.stem.porter import PorterStemmer #The PorterStemmer is a widely used stemming algorithm, which is used to reduce words to their base or root form.
 stemmer = PorterStemmer()
 
 
-def tokenize(sentence):
+def tokenize(sentence): #okenization is the process of breaking down a sentence or a piece of text into individual words or tokens. 
     """
     split sentence into array of words/tokens
     a token can be a word or punctuation character, or number
@@ -13,7 +13,7 @@ def tokenize(sentence):
     return nltk.word_tokenize(sentence)
 
 
-def stem(word):
+def stem(word): #Stemming helps to normalize words by reducing them to their base or root form
     """
     stemming = find the root form of the word
     examples:
@@ -24,7 +24,7 @@ def stem(word):
     return stemmer.stem(word.lower())
 
 
-def bag_of_words(tokenized_sentence, words):
+def bag_of_words(tokenized_sentence, words): #This function takes a tokenized sentence (a list of words) and a list of known words as input.
     """
     return bag of words array:
     1 for each known word that exists in the sentence, 0 otherwise
